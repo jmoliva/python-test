ebcf9ec801aa9a342c348979928653db19d9ee89
JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>
2021-10-13 14:37:00 +0200

    update with requests
    Signed-off-by: JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>

--- sample.py
+++ sample.py
@@ -5,9 +5,6 @@ host = socket.gethostname()
 now = datetime.datetime.now()
 nowS = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-import urllib3
-http=urllib3.PoolManager()
-url = 'http://Echo4Advini.eu-de.mybluemix.net/v1/echo?host=' + host + '&time=' + nowS
-r = http.request('GET', url)
-print r.status
-
+import requests
+r = requests.get('http://Echo4Advini.eu-de.mybluemix.net/v1/echo?host=' + host + '&time=' + nowS)
+print(r.status_code)
