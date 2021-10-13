5dd06e454c2b59bfd3bd1d5b017294879a322792
JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>
2021-10-13 14:53:22 +0200

    from codeengine
    Signed-off-by: JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>

--- sample.py
+++ sample.py
@@ -8,5 +8,5 @@ now = datetime.datetime.now()
 nowS = now.strftime("%m/%d/%Y, %H:%M:%S")
 
 import requests
-r = requests.get('http://Echo4Advini.eu-de.mybluemix.net/v1/echo?host=' + host + '&time=' + nowS)
+r = requests.get('http://Echo4Advini.eu-de.mybluemix.net/v1/echo?host-from-code_engine=' + host + '&time=' + nowS)
 print(r.status_code)
